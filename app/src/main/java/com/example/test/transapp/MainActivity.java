5edf2b98bd983f63a8f44d4e21fe8102574511d5
mudwell <mirainoeve@gmail.com>
2015-01-21 20:29:59 +0900

    音声認識追加

--- app/src/main/java/com/example/test/transapp/MainActivity.java
+++ app/src/main/java/com/example/test/transapp/MainActivity.java
@@ -1,19 +1,74 @@
 package com.example.test.transapp;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
+    private static final int REQUEST_CODE = 0;
+    private ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(adapter);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    // インテント作成
+                    Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH); // ACTION_WEB_SEARCH
+                    intent.putExtra(
+                            RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                            RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                    intent.putExtra(
+                            RecognizerIntent.EXTRA_PROMPT,
+                            "お話しください"); // お好きな文字に変更できます
+
+                    // インテント発行
+                    startActivityForResult(intent, REQUEST_CODE);
+                } catch (ActivityNotFoundException e) {
+                    // このインテントに応答できるアクティビティがインストールされていない場合
+                    Toast.makeText(MainActivity.this,
+                            "ActivityNotFoundException", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
+            String resultsString = "認識失敗";
+
+            // 結果文字列リスト
+            List<String> results = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+
+            if (results.size() > 0) {
+                resultsString = results.get(0);
+            }
+
+            adapter.add(resultsString);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
